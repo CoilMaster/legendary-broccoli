5f404ab6ec6745eaab65bb69da68059c1c67f994
gpodraza1 <g.podraza1@gmail.com>
2018-12-17 20:37:08 +0100

    dodawanie modeli

--- legendary_broccoli/broccoli/models.py
+++ legendary_broccoli/broccoli/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class coil(models.model):
-    name = model.Charfield(max)
+
+class Category(MPTTModel):
+    class Meta:
+        verbose_name = "Kategorie grzałek"
+        verbose_name_plural = "Kategorie grzałek"
+
+    name = model.Charfield(max_length=40, null=True, vorbose_name="Kategorie grzałek")
+
+    def __str__(self):
+        return self.name
+
+class Coil(models.Model):
+    class Meta:
+        verbose_name = "Grzałki"
+        verbose_name_plural = "Grzałki"
+
+    name = model.Charfield(max_length=40, null=True, vorbose_name="Nazwa")
+    category = model.TreeForeignKey(Category,
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              related_name='product_set',
+                              verbose_name="Kategoria")
+    spiral = model.IntegerField(verbose_name="Ilość zwoji")
+
+
+    def __str__(self):
+        return self.name
